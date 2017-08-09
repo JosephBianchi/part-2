fe9bb9493b473f59e7322310ae42091324a22067
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-09 17:58:49 -0400

    exc 5

---
+++ exc5.rb
@@ -0,0 +1,11 @@
+def greet_backwards(name)
+  p "hello #{name.reverse}! Welcome Home"
+end
+
+greet_backwards("Bob")
+
+greet_backwards("Shirly")
+
+greet_backwards("Sue")
+
+greet_backwards("Andy")
