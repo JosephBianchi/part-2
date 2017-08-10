8af46fc517d72a9d8882a0db339471b9f87bea7d
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-09 22:28:07 -0400

    need to finish 8

---
+++ exc8.rb
@@ -0,0 +1,43 @@
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+# Needs to be finished
+def info_collect(person_1)
+  put
+end
+
+def speed(distance, minutes)
+  return distance /(60 * minutes)
+end
+
+
+
+
+
+
+
+
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
